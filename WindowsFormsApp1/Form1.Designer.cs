f51143ec713cd9c77b6b8489d554d387b0b0e946
JMezyk <jordan.mezyk@wp.pl>
2024-11-28 15:31:35 +0100

    Cleaned code from unused functions and data

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -41,18 +41,7 @@ namespace WindowsFormsApp1
             this.item1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.item2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.item3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.contextMenuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -68,7 +57,7 @@ namespace WindowsFormsApp1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(391, 22);
+            this.button1.Location = new System.Drawing.Point(94, 335);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 26);
             this.button1.TabIndex = 1;
@@ -78,7 +67,7 @@ namespace WindowsFormsApp1
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(497, 22);
+            this.progressBar1.Location = new System.Drawing.Point(200, 335);
             this.progressBar1.Maximum = 1000;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(565, 26);
@@ -87,7 +76,7 @@ namespace WindowsFormsApp1
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(341, 22);
+            this.textBox1.Location = new System.Drawing.Point(44, 335);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(44, 26);
             this.textBox1.TabIndex = 3;
@@ -133,99 +122,11 @@ namespace WindowsFormsApp1
             this.item3ToolStripMenuItem.Text = "Item &3";
             this.item3ToolStripMenuItem.Click += new System.EventHandler(this.item3ToolStripMenuItem_Click);
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(540, 54);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(522, 162);
-            this.dataGridView1.TabIndex = 4;
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(12, 54);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(522, 162);
-            this.textBox2.TabIndex = 5;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 222);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(170, 40);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "ZAPISZ";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(540, 222);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(170, 40);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "WCZYTAJ";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(188, 222);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(170, 40);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "SERIALIZUJ XML";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(716, 222);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(170, 40);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "DESERIALIZUJ XML";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(364, 222);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(170, 40);
-            this.button6.TabIndex = 9;
-            this.button6.Text = "SERIALIZUJ JSON";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(892, 222);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(170, 40);
-            this.button7.TabIndex = 10;
-            this.button7.Text = "DESERIALIZUJ JSON";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1073, 587);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
@@ -234,7 +135,6 @@ namespace WindowsFormsApp1
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.contextMenuStrip1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -251,16 +151,6 @@ namespace WindowsFormsApp1
         private ToolStripMenuItem item1ToolStripMenuItem;
         private ToolStripMenuItem item2ToolStripMenuItem;
         private ToolStripMenuItem item3ToolStripMenuItem;
-        private DataGridView dataGridView1;
-        private OpenFileDialog openFileDialog1;
-        private TextBox textBox2;
-        private SaveFileDialog saveFileDialog1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -20,28 +20,23 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        #region FieldDefinitions
-        static int Licznik;
+         static int Licznik;
             System.Timers.Timer tTick;
-            public DaneXML obiektXML;
-            public DaneJSON obiektJSON;
-        #endregion
+           
+     
       
-        #region Constructors
+        
         public Form1()
         {
             InitializeComponent();
-            populateGridView();
             SetTimer();
-            this.obiektXML = new DaneXML();
-            this.obiektJSON = new DaneJSON();
         }
 
         ~Form1()
         {
             
         }
-        #endregion
+       
         
         #region HelloWorld
         private void SetTimer()
@@ -100,38 +95,7 @@ namespace WindowsFormsApp1
         }
         #endregion
         
-        #region FileReadWrite
-        private void populateGridView()
-        {
-            string[] row0 = { "11/22/1968",     "29", "Revolution 9",                       "Beatles",          "The Beatles [White Album]" };
-            string[] row1 = { "1960",           "6",  "Fools Rush In",                      "Frank Sinatra",    "Nice 'N' Easy" };
-            string[] row2 = { "11/11/1971",     "1",  "One of These Days",                  "Pink Floyd",       "Meddle" };
-            string[] row3 = { "1988",           "7",  "Where Is My Mind?",                  "Pixies",           "Surfer Rosa" };
-            string[] row4 = { "5/1981",         "9",  "Can't Find My Mind",                 "Cramps",           "Psychedelic Jungle" };
-            string[] row5 = { "6/10/2003",      "13", "Scatterbrain. (As Dead As Leaves.)", "Radiohead",        "Hail to the Thief" };
-            string[] row6 = { "6/30/1992",      "3",  "Dress",                              "P J Harvey",       "Dry" };
-
-            dataGridView1.ColumnCount       = 5;
-            dataGridView1.Columns[0].Name   = "Release Date";
-            dataGridView1.Columns[1].Name   = "Track";
-            dataGridView1.Columns[2].Name   = "Title";
-            dataGridView1.Columns[3].Name   = "Artist";
-            dataGridView1.Columns[4].Name   = "Album";
-
-            dataGridView1.Rows.Add(row0);
-            dataGridView1.Rows.Add(row1);
-            dataGridView1.Rows.Add(row2);
-            dataGridView1.Rows.Add(row3);
-            dataGridView1.Rows.Add(row4);
-            dataGridView1.Rows.Add(row5);
-            dataGridView1.Rows.Add(row6);
-
-            dataGridView1.Columns[0].DisplayIndex = 3;
-            dataGridView1.Columns[1].DisplayIndex = 4;
-            dataGridView1.Columns[2].DisplayIndex = 0;
-            dataGridView1.Columns[3].DisplayIndex = 1;
-            dataGridView1.Columns[4].DisplayIndex = 2;
-        }
+            
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -143,163 +107,7 @@ namespace WindowsFormsApp1
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)  // ZAPISZ
-        {
-            StreamWriter myStream;
-            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            saveFileDialog1.FilterIndex = 2;
-            saveFileDialog1.RestoreDirectory = true;
-           
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                if (saveFileDialog1.FileName != null)
-                {
-                    myStream = new StreamWriter(saveFileDialog1.FileName,false);
-                    
-                    myStream.Write(textBox2.Text);
-                    myStream.Close();
-                }
-            }
 
-        }
-
-        private void button3_Click(object sender, EventArgs e)  // WCZYTAJ 
-        {
-            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 2;
-            openFileDialog1.RestoreDirectory = true;
-            string[] buffer;
-            string[] fields;
-
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                if (openFileDialog1.FileName != null)
-                {
-
-                    buffer = File.ReadAllLines(openFileDialog1.FileName);
-                    textBox2.Clear();
-                    foreach (string line in buffer)
-                        textBox2.AppendText(line + "\r\n");
-                    
-                    dataGridView1.Columns.Clear();
-                    dataGridView1.ColumnCount = 3;
-                    dataGridView1.Columns[0].Name = "Text_col1";
-                    dataGridView1.Columns[1].Name = "Text_col2";
-                    dataGridView1.Columns[2].Name = "Text_col3";
-
-                    foreach (string line in buffer)
-                    {
-                        fields = line.Split('\t');
-                        dataGridView1.Rows.Add(fields);
-                    }
-
-                }
-            }
-
-
-        }
-        #endregion
-
-        #region Serialization
-        private void button4_Click(object sender, EventArgs e) // SERIALIZUJ
-        {
-            StreamWriter myStream;
-            XmlSerializer mySerializer;
-            
-            saveFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
-            saveFileDialog1.FilterIndex = 0;
-            saveFileDialog1.RestoreDirectory = true;
-
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK) 
-            {
-                if (saveFileDialog1.FileName != null)
-                {
-                    mySerializer = new XmlSerializer(obiektXML.GetType());
-                    myStream = new StreamWriter(saveFileDialog1.FileName, false);
-                    mySerializer.Serialize(myStream,obiektXML);
-                    myStream.Close();
-                
-                  
-                }
-                
-                
-            }
-
-
-        }
-
-        private void button5_Click(object sender, EventArgs e)      // DESERIALIZUJ
-        {
-            openFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 0;
-            openFileDialog1.RestoreDirectory = true;
-            DaneXML inputData;
-            XmlSerializer mySerializer;
-            FileStream myStream;
-
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                if (openFileDialog1.FileName != null)
-                {
-                    inputData = new DaneXML();
-                    mySerializer = new XmlSerializer(obiektXML.GetType());
-                    myStream = new FileStream(openFileDialog1.FileName, FileMode.Open);
-                    inputData = (DaneXML)mySerializer.Deserialize(myStream);
-
-                    textBox2.Text = inputData.ToString();
-                }
-            }
-        }
-
-        private void button6_Click(object sender, EventArgs e)      // DO JSONa
-        {
-            StreamWriter myStream;
-            JsonSerializerOptions options;
-
-            saveFileDialog1.Filter = "json files (*.json)|*.json|All files (*.*)|*.*";
-            saveFileDialog1.FilterIndex = 0;
-            saveFileDialog1.RestoreDirectory = true;
-
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                if (saveFileDialog1.FileName != null)
-                {
-                    myStream = new StreamWriter(saveFileDialog1.FileName, false);
-                    options = new JsonSerializerOptions();
-                    options.WriteIndented = true;
-                    myStream.Write(JsonSerializer.Serialize(obiektJSON, options));
-                    myStream.Close();
-                }
-            }
-        }
-
-        private void button7_Click(object sender, EventArgs e)          // DESERIALIZUJ JSON
-        {
-            openFileDialog1.Filter = "json files (*.json)|*.json|All files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 0;
-            openFileDialog1.RestoreDirectory = true;
-            DaneJSON inputData;
-            string JSONbuffer;
-
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                if (openFileDialog1.FileName != null)
-                {
-                    inputData = new DaneJSON();
-                    JSONbuffer = File.ReadAllText(openFileDialog1.FileName);
-                    inputData = JsonSerializer.Deserialize<DaneJSON>(JSONbuffer);
-                    textBox2.Text = inputData.ToString();
-                }
-            }
-        }
-        #endregion
-
-        #region DataBaseConnectivity
-        private void DBconnect() 
-        {
-            Console.WriteLine( "Nice try asshole ;)");
-            return; 
-        }
-        #endregion
+       
     }
 }
